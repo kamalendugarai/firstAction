042d0d260abba984688d838f5711bdeea5abad13
Kamalendu Garai <kamalendugarai@gmail.com>
2024-11-09 17:15:51 +0530

    Modified action file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,25 +8,13 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// export default defineConfig({
-//   webServer: {
-//     command: 'npm run start',
-//     url: 'http://127.0.0.1:3000',
-//     timeout: 120 * 1000,
-//     reuseExistingServer: !process.env.CI,
-//   },
-//   use: {
-//     baseURL: 'http://localhost:3000/',
-//   },
-// });
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -38,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://127.0.0.1:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
